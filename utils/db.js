d3ac8f1b13a57844b20696b3143d8cfcd95911a4
aashish <ritikkamboj4314@gmail.com>
2026-02-08 13:40:53 +0530

    created client for mongodb and redis connection

---
+++ utils/db.js
@@ -0,0 +1,13 @@
+    const mongoose = require("mongoose");
+
+    const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDb connected Successfully");
+    } catch (error) {
+        console.log("MongoDb connection failed ", error);
+        process.exit(1);
+    }
+    };
+
+    module.exports = connectDB;
---
+++ utils/redisClient.js
@@ -0,0 +1,20 @@
+const redis = require("redis");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const redisClient = new redis.createClient({
+    url : process.env.REDIS_URI
+});
+
+redisClient.on("connect", ()=>
+{
+    console.log("connected to redis");
+}) ;
+redisClient.on("error",(error)=>{
+    console.log("Connection to redis failed", error);
+})
+
+redisClient.connect();
+
+module.exports = {redisClient} ;
