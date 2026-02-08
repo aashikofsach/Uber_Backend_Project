baf8c4351f4d7f04d7d2b4d53ed02a0558d57b04
aashish <ritikkamboj4314@gmail.com>
2026-02-08 09:38:27 +0530

    created userSchema and bookingSchema

---
+++ models/booking.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  passenger: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  driver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  source: {
+    longitude: {
+      type: Number,
+    },
+    latitude: {
+      type: Number,
+    },
+  },
+  destination: {
+    longitude: {
+      type: Number,
+    },
+    latitude: {
+      type: Number,
+    },
+  },
+  fare: Number,
+  status: {
+    type: Number,
+    enum: ["pending", "confirmed", "completed"],
+    default: "pending",
+  },
+  rating: Number,
+  feedback: String,
+});
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["passenger", "driver"] },
+  location : {
+    type : {
+        type : String,
+        enum : ['Point'],
+        default : 'Point'
+    },
+    coordinates : {
+        type : [Number],
+        default : [0,0]
+    }
+  }
+});
+
+// this is a middleware which runs before saving the password 
+userSchema.pre("save", async function(){
+    if(!this.isModified("password")) next() ;
+    this.password = await bcrypt(this.password , 10);
+    next();
+
+})
+
+userSchema.methods.comparePassword = async function(password)
+{
+    return bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports=User;
