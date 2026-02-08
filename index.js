06927b5322c4ed48e10e90aaf0fe89946217b08f
aashish <ritikkamboj4314@gmail.com>
2026-02-08 14:29:56 +0530

    coded index.js

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const http = require("http");
+const mongoose = require("mongoose");
+const cors = require('cors');
 
-console.log(mongoose.version)
+const authRoute = require("./routes/authRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const driverRoutes = require("./routes/driverRoutes");
+const passengerRoutes = require("./routes/passengerRoutes");
+const {redisClient} = require("./utils/redisClient");
+
+dotenv.config();
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static());
+
+mongoose.connect(process.env.MONGO_URI);
+app.use("api/auth", authRoute);
+app.use("api/bookings", bookingRoutes(io));
+app.use("api/drivers", driverRoutes);
+app.use("api/passengers", passengerRoutes(io));
+
+server.listen(process.env.PORT, () => {
+  console.log("server is lsitening on port", process.env.PORT);
+});
+
+redisClient.on("connect", ()=>
+{
+    console.log("connected to redis")
+})
